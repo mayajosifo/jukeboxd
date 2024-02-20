3879f84b125df0883c507bab6b8b9d55d7ea2cf1
Maya Josifovska <mayajosifovska@Mayas-MacBook-Pro.local>
2024-02-19 20:20:01 -0800

    added comments

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,17 @@ const app = express();
 // use port number stored in env or use 3000
 const PORT = process.env.PORT || 3000;
 
-// most basic route
+// route (works based on user requests and site responses)
+// this route starts at the root/main entry point of the website
 app.get('/', (req, res) => {
+
+    // sends a response that says 'welcome to jukeboxd'
   res.send('welcome to jukeboxd');
+
 });
 
+// tells the app to begin listening to requests from the specified port number
 app.listen(PORT, () => {
+    // this message is just here to let you know the server started successfully
   console.log(`Server is running on port ${PORT}`);
 });
