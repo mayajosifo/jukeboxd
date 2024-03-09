9e13299fac5007f8689078e1ad718e7e57ab9997
garrattarmy <armygarratt@gmail.com>
2024-03-09 15:37:11 -0800

    Updated routing so searches are on userpage

--- skeleton/src/pages/UserPage.js
+++ skeleton/src/pages/UserPage.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import SearchAlbum from '../components/SearchAlbum'; 
+import SearchUser from '../components/SearchUser';
+import Navbar from '../components/Navbar';
+//import { Route, Routes, Navigate } from 'react-router-dom';
 
 function userPage() {
 
     return (
-        <div>
-            <h1>fuck yea</h1>
-        </div>
-    )
+          <div className="App">
+            <Navbar />
+            <header className="App-header">
+              <h1>Album Finder</h1>
+            </header>
+            <main>
+              <SearchAlbum />
+              <SearchUser />
+            </main>
+          </div>
+      );
 }
 
 
