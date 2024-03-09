4ad17f09f0ecbb322a5bd70336a0f7f14bb1c9ef
John Reinker <reinkera123@g.ucla.edu>
2024-03-09 13:07:18 -0800

    Changes to App.js and a few others

--- skeleton/src/App.js
+++ skeleton/src/App.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+
 import './App.css';
-import SearchAlbum from './components/SearchAlbum'; // Adjust the path as necessary
+
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import UserPage from './pages/UserPage';
+
+
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false)
+  
+
+
+
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<SignInPage setIsSignedIn={setIsSignedIn}/>} />
+        <Route path='/sign-up' element={<SignUpPage setIsSignedIn={setIsSignedIn}/>} />
+        <Route 
+          path='/user-page' 
+          element={isSignedIn ? <UserPage /> : <Navigate replace to='/' />}
+          />
+   
+      
+      </Routes>
+      
+    </div>
+
+
+
+  )
+
+
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +46,7 @@ function App() {
       </main>
     </div>
   );
+  */
 }
 
 export default App;
