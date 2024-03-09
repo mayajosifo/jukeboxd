be3ed2c400b67a1b0c35b03341c22662823f31d2
John Reinker <reinkera123@g.ucla.edu>
2024-03-09 13:17:01 -0800

    Removed a lot of garbage files that arent needed

---
+++ skeleton/src/components/CreateUser.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+import {db} from "../config/firebase";
+import {doc, setDoc} from "firebase/firestore";
+
+const CreateUser = () => {
+    
+  };
+  
+  export default CreateUser;
