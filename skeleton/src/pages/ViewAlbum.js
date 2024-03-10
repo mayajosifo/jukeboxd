b53087ea870b640edf15ca2e827846eab1c895b5
garrattarmy <armygarratt@gmail.com>
2024-03-10 16:56:52 -0700

    Clicking on an album displays its info now

--- skeleton/src/pages/ViewAlbum.js
+++ skeleton/src/pages/ViewAlbum.js
@@ -2,7 +2,7 @@
 import AddReviewForm from '../components/AddReviewForm';
 import { useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, doc, getDoc, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../config/firebase';
 import styles from './ViewAlbum.module.css';
 
@@ -10,6 +10,7 @@ import styles from './ViewAlbum.module.css';
 function ViewAlbum({userId}) {
     const [showReview, setShowReview] = useState(false);
     const [albumId, setAlbumId] = useState('');
+    const [album, setAlbum] = useState(null);
     const [reviews, setReviews] = useState([]);
 
     const { id } = useParams(); 
@@ -17,15 +18,24 @@ function ViewAlbum({userId}) {
 
     useEffect(() => {
         // Set the albumId state variable when the component mounts
-        const fetchReviews = async () => {
+        const fetchAlbumAndReviews = async () => {
+
+            const albumDoc = doc(db, 'albums', id);
+            const albumSnapshot = await getDoc(albumDoc);
+            if (albumSnapshot.exists()) {
+                setAlbum(albumSnapshot.data());
+            } else {
+                console.log('Album not found');
+            }
+
             const q = query(collection(db, "reviews"), where("albumsId", "==", id));
             const querySnapshot = await getDocs(q);
             const fetchedReviews = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
             setReviews(fetchedReviews);
+            setAlbumId(id);
         };
 
-        fetchReviews();
-        setAlbumId(id);
+        fetchAlbumAndReviews();
     }, [id]);                       
 
     const handleButtonClick = () => {
@@ -42,7 +52,15 @@ function ViewAlbum({userId}) {
             <h1>Album Review Page</h1>
             <button onClick={handleButtonClick}>Add Review</button>
             {showReview && <AddReviewForm onClose={handleCloseForm} albumId={albumId} />}
-            <h2>Reviews for Album ID: {albumId}</h2>
+            {album && (
+                <div>
+                    <h2>Name: {album.albumName}</h2>
+                    <p>Artist: {album.artistName}</p>
+                    {album.coverUrl && <img src={album.coverUrl} alt="Album Cover" style={{ width: 300, height: 300 }} />}
+                    <p>Year: {album.releaseYear}</p>
+                </div>
+            )}
+            <h2>Reviews: </h2>
             <div className={styles.reviewsContainer}>
                 {reviews.map(review => (
                     <div key={review.id} className={styles.reviewItem}>
@@ -52,7 +70,6 @@ function ViewAlbum({userId}) {
                     </div>
                 ))}
             </div>
-            <h1>{albumId}</h1>
         </div>
     );
 }
