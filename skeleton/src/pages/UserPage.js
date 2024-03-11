84fe18463b4be219b89fdbb6ff0ca3ea47008ca8
garrattarmy <armygarratt@gmail.com>
2024-03-10 20:52:44 -0700

    UserPage now has access to User data, including follower/following list

--- skeleton/src/pages/UserPage.js
+++ skeleton/src/pages/UserPage.js
@@ -1,4 +1,7 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
+import { collection, doc, getDoc, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../config/firebase';
 
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import SearchAlbum from '../components/SearchAlbum'; 
@@ -8,15 +11,38 @@ import AddReviewForm from '../components/AddReviewForm';
 
 //import { Route, Routes, Navigate } from 'react-router-dom';
 
-function userPage({userId}) {
+function UserPage({userId}) {
+  const [user, setUser] = useState(null);
 
-    return (
+  useEffect(() => {
+    // Set the albumId state variable when the component mounts
+    const fetchUser = async () => {
+
+        const userDoc = doc(db, 'users', userId);
+        const userSnapshot = await getDoc(userDoc);
+        if (userSnapshot.exists()) {
+            setUser(userSnapshot.data());
+        } else {
+            console.log('User not found');
+        }
+    };
+
+    fetchUser();
+  });       
+
+
+  return (
           <div className="App">
             <header className="App-header">
-              <h1>Album Finder</h1>
+              <h1>Profile</h1>
+              <p>User ID: {userId}</p>
             </header>
             <main>
-              <AddReviewForm />
+              {user && (
+                <div>
+                  <h1>Username: {user.userName}</h1>
+                </div>
+              )}
             </main>
           </div>
       );
@@ -24,4 +50,4 @@ function userPage({userId}) {
 
 
 
-export default userPage;
+export default UserPage;
