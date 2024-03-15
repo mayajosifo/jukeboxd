95d7d375a6d1e0df0bbdc3bc99e0a77b7c8d09e0
John Reinker <reinkera123@g.ucla.edu>
2024-03-14 19:35:45 -0700

    Removed unneccesary folders and renamed the folder skeleton to jukeboxd app

--- backend/controllers/reviewController.js
+++
@@ -1,3 +0,0 @@
-// Controller files are used to define routes without bloating the route file
-// When we have a database set up, define the routers here
-// https://www.youtube.com/watch?v=oEHHjs1UVXQ&list=PL4cUxeGkcC9iJ_KkrkBZWZRHVwnzLIoUE&index=6
--- backend/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-// login user
-const loginUser = async(req, res) => {
-    res.json({mssg: 'login user'})
-}
-
-const signupUser = async(req, res) => {
-    res.json({mssg: 'signup user'})
-}
