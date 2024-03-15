842601d846ef9864751cd517f5740ccb80042559
Leah George <leahgeorge2022@gmail.com>
2024-03-14 17:54:22 -0700

    added delete alert

--- skeleton/src/components/Review.js
+++ skeleton/src/components/Review.js
@@ -5,26 +5,27 @@ import './Review.css'; // Importing the CSS file
 const Review = ({ review, userName }) => { 
     return (
         <div className="review-container">
-            {review.album && (
-                <div className="album-info-container">
-                    {review.album.coverUrl && (
-                        <div className="album-cover-container">
-                            <Link to={`/album/${review.album.id}`}>
-                                <img src={review.album.coverUrl} alt="Album Cover" className="album-cover"/>
-                            </Link>
-                        </div>
-                    )}
-                    <Link to={`/album/${review.album.id}`} className="album-title-link">
-                        <h4 className="album-title">{review.album.albumName}</h4> {/* Added album title */}
+    {review.album && (
+        <div className="album-info-container">
+            {review.album.coverUrl && (
+                <div className="album-cover-container">
+                    <Link to={`/album/${review.album.id}`}>
+                        <img src={review.album.coverUrl} alt="Album Cover" className="album-cover"/>
                     </Link>
                 </div>
             )}
-            <div className="review-content">
-                <h3>Review by: {userName}</h3>
-                <p className="review-rating">Rating: {review.rating}/10</p>
-                <p className="review-text">{review.reviewText}</p>
-            </div>
+            <Link to={`/album/${review.album.id}`} className="album-title-link">
+                <h4 className="album-title">{review.album.albumName}</h4> 
+            </Link>
         </div>
+    )}
+    <div className="review-content" style={{ marginLeft: '30px' }}> 
+        <h3>Review by: {userName}</h3>
+        <p className="review-rating">Rating: {review.rating}/10</p>
+        <p className="review-text">{review.reviewText}</p>
+    </div>
+</div>
+
     );
 };
 
